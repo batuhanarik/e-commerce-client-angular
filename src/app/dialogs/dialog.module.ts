c994befc3ba28a117ccc0fa56c5434e4a4404d83
BATUHAN\batuh <batuhanarik123@gmail.com>
2023-12-06 19:16:04 +0300

    some imports

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -4,15 +4,11 @@ import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 import { FileUploadDialogComponent } from './file-upload-dialog/file-upload-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { FileUploadModule } from '../services/common/file-upload/file-upload.module';
 
 @NgModule({
-  declarations: [DeleteDialogComponent,FileUploadDialogComponent],
-  imports: [
-    CommonModule,
-    MatDialogModule,
-    MatButtonModule
-  ]
+  declarations: [DeleteDialogComponent, FileUploadDialogComponent],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatCardModule],
 })
-export class DialogModule { }
+export class DialogModule {}
